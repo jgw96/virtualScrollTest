e41d4f0992961c4aa38e066159bbaf279d23f730
Justin Willis <jgw9617@gmail.com>
2016-12-22 14:17:59 -0600

    add ion-refresh

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -24,4 +24,13 @@ export class Page1 {
     };
   }
 
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+  }
+
 }
